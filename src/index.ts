433f99e17cbbe2fd744e6c9dabf4901862bee7d7
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2019-07-16 08:40:47 -0300

    feat(schema): write schema.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import path from "path";
 import fs from "fs";
+import { promisify } from 'util';
 import { graphql2ts } from './graphql2ts';
+
+const writeFileAsync = promisify(fs.writeFile);
 
 const cwd = process.cwd();
 
@@ -17,4 +20,6 @@ const cwd = process.cwd();
   const result = await graphql2ts(source);
 
   console.log('result: ', result);
+
+  await writeFileAsync(path.join(cwd, 'schema.ts'), result);
 })();
