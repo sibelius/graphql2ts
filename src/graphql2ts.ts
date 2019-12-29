94950a1837d4bf4bf6db52c4ace447a073eac594
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2019-12-29 11:37:33 -0300

    chore: general code improvements

--- src/graphql2ts.ts
+++ src/graphql2ts.ts
@@ -1,128 +1,19 @@
-import {
-  parse,
-  DocumentNode,
-  ObjectTypeDefinitionNode,
-  InputObjectTypeDefinitionNode,
-  EnumTypeDefinitionNode,
-  UnionTypeDefinitionNode,
-  InterfaceTypeDefinitionNode,
-  FieldDefinitionNode,
-  TypeNode,
-  StringValueNode, InputValueDefinitionNode
-} from 'graphql';
-import {parse as babelParser } from '@babel/parser';
-import generate from '@babel/generator';
-import prettier from "prettier";
+import { parse, DocumentNode } from 'graphql';
+import prettier from 'prettier';
 
-export const typedNode = (type: TypeNode) => {
-  switch (type.kind) {
-    case 'NamedType':
-      return type.name.value;
-    case 'ListType':
-      return `GraphQLList(${typedNode(type.type)})`;
-    case 'NonNullType':
-      return `GraphQLNonNull(${typedNode(type.type)})`;
-    default:
-      return '';
-  }
-};
-
-export const descriptionDefinition = (description?: StringValueNode) => {
-  return description ? `description: '${description.value}',` : '';
-};
-
-export const inputValueDefinitionNode = (input: InputValueDefinitionNode) => {
-  const { name, description, type } = input;
-
-  const code = `
-    ${name.value}: {
-      type: ${typedNode(type)},
-      ${descriptionDefinition(description)}
-    },
-  `;
-
-  return code;
-};
-
-export const fieldDefinition = (field: FieldDefinitionNode) => {
-  const { name, description, type } = field;
-
-  const argCode = field.arguments.map(arg => inputValueDefinitionNode(arg));
-
-  const args = argCode.length > 0
-    ? `
-      args: {
-        ${argCode.join('\n')}
-      }
-    ` : '';
-
-  const code = `
-    ${name.value}: {
-      type: ${typedNode(type)},
-      ${descriptionDefinition(description)}
-      ${args}
-    },
-  `;
-
-  return code;
-};
-
-export const objectTypeDefinition = (definition: ObjectTypeDefinitionNode) => {
-  const { name, description, fields } = definition;
-
-  const fieldsCode = fields.map(field => fieldDefinition(field)).join('\n');
-
-  const code = `
-    const ${name.value} = new GraphQLObjectType({
-      name: '${name.value}',
-      ${descriptionDefinition(description)}
-      fields: () => ({
-        ${fieldsCode}
-      })
-    });
-  `;
-  const ast = babelParser(code);
-
-  const output = generate(ast);
-
-  return output.code;
-};
-
-export const inputObjectTypeDefinition = (definition: InputObjectTypeDefinitionNode) => {
-  return '';
-};
-
-export const enumTypeDefinition = (definition: EnumTypeDefinitionNode) => {
-  return '';
-};
-
-export const unionTypeDefinition = (definition: UnionTypeDefinitionNode) => {
-  return '';
-};
-
-export const interfaceTypeDefinition = (definition: InterfaceTypeDefinitionNode) => {
-  return '';
-};
-
-const DEFINITION_TO_TS = {
-  ObjectTypeDefinition: objectTypeDefinition,
-  InputObjectTypeDefinition: inputObjectTypeDefinition,
-  EnumTypeDefinition: enumTypeDefinition,
-  UnionTypeDefinition: unionTypeDefinition,
-  InterfaceTypeDefinition: interfaceTypeDefinition,
-};
+import { definitions } from './definitions';
 
 export const graphql2ts = (source: string) => {
   const document: DocumentNode = parse(source);
 
-  const definitionsTs = document.definitions.slice(0,5).map(definition => {
-    if (definition.kind in DEFINITION_TO_TS) {
-      return DEFINITION_TO_TS[definition.kind](definition);
+  const definitionsTs = document.definitions.slice(0, 5).map((definition) => {
+    if (definition.kind in definitions) {
+      return definitions[definition.kind](definition);
     }
 
-    console.log('not implemented for: ', definition.kind);
-    return ''
+    console.error('not implemented for: ', definition.kind);
+    return '';
   });
 
-  return prettier.format(definitionsTs.join('\n'));
+  return prettier.format(definitionsTs.join('\n'), { parser: 'babel' });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-import path from "path";
-import fs from "fs";
-import { promisify } from 'util';
-import { graphql2ts } from './graphql2ts';
+import { run } from './cli';
 
-const writeFileAsync = promisify(fs.writeFile);
-
-const cwd = process.cwd();
-
-(async () => {
-  // if (process.argv.length !== 4) {
-  //   console.log(`usage: ${process.argv[0]} ${process.argv[1]} schema.graphql`);
-  //   return;
-  // }
-
-  const filename = path.join(cwd, process.argv[3] || 'schema.graphql');
-
-  const source = fs.readFileSync(filename, { encoding: 'utf8' });
-
-  const result = await graphql2ts(source);
-
-  console.log('result: ', result);
-
-  await writeFileAsync(path.join(cwd, 'schema.ts'), result);
-})();
+run();
